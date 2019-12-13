bb3be9bb0ebcc00c672c4ee6ce22678158d9f261
patheticcockroach <patheticcockroach@patheticcockroach.com>
2019-12-13 11:56:40 +0100

    Remove pre-build 1045 url format

--- src/freenet/clients/http/FProxyToadlet.java
+++ src/freenet/clients/http/FProxyToadlet.java
@@ -523,12 +523,6 @@ public final class FProxyToadlet extends Toadlet implements RequestClient {
 		}else if(ks.equals("/robots.txt") && ctx.doRobots()){
 			this.writeTextReply(ctx, 200, "Ok", "User-agent: *\nDisallow: /");
 			return;
-		}else if(ks.startsWith("/darknet/") || ks.equals("/darknet")) { //TODO (pre-build 1045 url format) remove when obsolete
-			writePermanentRedirect(ctx, "obsoleted", "/friends/");
-			return;
-		}else if(ks.startsWith("/opennet/") || ks.equals("/opennet")) { //TODO (pre-build 1045 url format) remove when obsolete
-			writePermanentRedirect(ctx, "obsoleted", "/strangers/");
-			return;
 		} else if(ks.startsWith("/queue/")) {
 			writePermanentRedirect(ctx, "obsoleted", "/downloads/");
 			return;
